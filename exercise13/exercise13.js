144e93d127e3d7ef0ff4737b5347327d463fb338
Maggie Dong <56137517+qingyangdong@users.noreply.github.com>
2021-11-20 03:56:47 -0500

    Add files via upload

---
+++ exercise13/exercise13.js
@@ -0,0 +1,31 @@
+var line1 = document.getElementById('line1');
+var line2 = document.getElementById('line2');
+var line3 = document.getElementById('line3');
+var line4 = document.getElementById('line4');
+
+line1.addEventListener('animationstart', function(){
+  line2.style.opacity = '0';
+  line3.style.opacity = '0';
+  line4.style.opacity = '0';
+})
+
+line1.addEventListener('animationend', function(){
+  line1.className = '';
+  line2.className = 'line_animate_on_event';
+})
+
+line2.addEventListener('animationend', function(){
+  line2.className = '';
+  line2.style.opacity = '1';
+  line3.className = 'line_animate_on_event';
+})
+
+line3.addEventListener('animationend', function(){
+  line3.className = '';
+  line3.style.opacity = '1';
+  line4.className = 'line_animate_on_event';
+})
+
+line4.addEventListener('animationend', function(){
+  line4.style.opacity = '1';
+})
