3aa6eb91e1bf71597c5cd6c0f201bf191baffe7a
dqymaggie <dqy@uw.edu>
2024-08-18 17:52:33 -0700

    icon switch for light and dark mode

--- portfolio.js
+++ portfolio.js
@@ -5,5 +5,18 @@ function darkMode() {
     element.classList.toggle('dark-mode');
 
     var icon = document.getElementById('darkmode-icon');
-    
+    // if (element.classList.contains('dark-mode')) {
+    //     icon.classList.remove('fa-moon');
+    //     icon.classList.add('fa-sun');
+    // } else {
+    //     icon.classList.remove('fa-sun');
+    //     icon.classList.add('fa-moon');
+    // }
+    if (element.classList.contains('dark-mode')) {
+        icon.classList.remove('bi-moon-fill');
+        icon.classList.add('bi-sun');
+    } else {
+        icon.classList.remove('bi-sun');
+        icon.classList.add('bi-moon-fill');
+    }
 }
