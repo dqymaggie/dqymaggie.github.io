20729e6b7866c9d7ede7312054a9c5569b194623
dqymaggie <dqy@uw.edu>
2024-08-19 17:11:58 -0700

    make header responsive

--- portfolio.js
+++ portfolio.js
@@ -1,17 +1,8 @@
-// var element = document.getElementById('darkmode');
-
 function darkMode() {
     var element = document.body;
     element.classList.toggle('dark-mode');
 
     var icon = document.getElementById('darkmode-icon');
-    // if (element.classList.contains('dark-mode')) {
-    //     icon.classList.remove('fa-moon');
-    //     icon.classList.add('fa-sun');
-    // } else {
-    //     icon.classList.remove('fa-sun');
-    //     icon.classList.add('fa-moon');
-    // }
     if (element.classList.contains('dark-mode')) {
         icon.classList.remove('bi-moon-fill');
         icon.classList.add('bi-sun');
